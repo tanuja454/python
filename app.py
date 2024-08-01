53015af7a1b00f1ce48ea9c35d5b911141cea5d7
tanuja454 <chennapragadalaxminagatanuja@gmail.com>
2024-08-01 12:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import boto3
-client = boto3.client('ec2')
-response = client.stop_instances(
-    InstanceIds=[
-        'i-06ede22472f69b87c',
-    ]
-)
-}
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
